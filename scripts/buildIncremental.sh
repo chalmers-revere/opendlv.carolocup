5954af4c9ba46ef77929ea13a2136e480f7d3160
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-09-16 14:16:41 +0200

    setting scripts

--- scripts/buildIncremental.sh
+++ scripts/buildIncremental.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #if you dont have xterm on your linux distro, run "sudo apt install xterm"
 
-#xterm -hold -e "cd $HOME/CaroloCup/opendlv.carolocup/docker && make updateDockerBaseImage && make buildIncremental && make createDockerImage && make removeNoneImagesFromDocker" &
+xterm -hold -e "cd $HOME/CaroloCup/opendlv.carolocup/docker && make updateDockerBaseImage && make buildIncremental && make createDockerImage && make removeNoneImagesFromDocker" &
 
-cd $HOME/CaroloCup/opendlv.carolocup/docker && make updateDockerBaseImage && make buildIncremental && make createDockerImage && make removeNoneImagesFromDocker
--- scripts/cleanAll.sh
+++ scripts/cleanAll.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#if you dont have xterm on your linux distro, run "sudo apt install xterm"
 
 xterm -hold -e "cd $HOME/CaroloCup/opendlv.carolocup/docker && make cleanAll" &
 
