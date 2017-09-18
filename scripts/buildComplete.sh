32740144367de444a05d300a37be0b1788a71651
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-09-18 23:52:26 +0200

    setting lidar lib * to be improved *

--- scripts/buildComplete.sh
+++ scripts/buildComplete.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#if you dont have xterm on your linux distro, run "sudo apt install xterm"
 
+#script for building complete
+#if you dont have xterm on your linux distro, run "sudo apt install xterm"
 #xterm -hold -e "cd $HOME/CaroloCup/opendlv.carolocup/docker && make updateDockerBaseImage && make buildComplete && make createDockerImage && make removeNoneImagesFromDocker" &
 
 cd $HOME/CaroloCup/opendlv.carolocup/docker && make updateDockerBaseImage && make buildComplete && make createDockerImage && make removeNoneImagesFromDocker
--- scripts/stopcontainers.sh
+++ scripts/stopcontainers.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#if you dont have xterm on your linux distro, run "sudo apt install xterm"
 
 docker stop	$(docker ps -a -q)
 
