cc65679e8a4a00d5c538fc8b8643685bb6f8ba62
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-11-14 03:04:58 +0100

    updating scripts

--- scripts/preBuildComplete.sh
+++ scripts/preBuildComplete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-trap "./buildComplete.sh" SIGINT
+trap "./buildComplete.sh" SIGTERM
 
 cd $HOME/CaroloCup/opendlv.carolocup/docker && make cleanAll &&
 docker stop	$(docker ps -a -q) &&
