0149e8aba5068837856185f833a320cc1e27eeb7
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-11-15 15:12:13 +0100

    scripts updates

---
+++ scripts/gitclonegap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd $HOME/CaroloCup/ && git clone https://github.com/chalmers-revere/opendlv.carolocup.git && git checkout gap-dev
--- usecases/cockpit/run.sh
+++ usecases/cockpit/run.sh
@@ -5,5 +5,5 @@ trap "docker-compose down" SIGINT
 
 cd $HOME/CaroloCup/opendlv.carolocup/docker && make updateDockerBaseImage && make buildIncremental && make createDockerImage && make removeNoneImagesFromDocker &&
 
-cd $cwd && xhost + && docker-compose down && docker-compose up --build >> log.txt
+cd $cwd && xhost + && docker-compose down && docker-compose up --build
 
