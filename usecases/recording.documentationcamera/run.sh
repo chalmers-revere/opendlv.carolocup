755b2b63a4437fd58e68c4b651cd27d50bcd843d
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-11-18 15:32:11 +0100

    update

--- usecases/recording.documentationcamera/run.sh
+++ usecases/recording.documentationcamera/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cwd=$(pwd)
+d=$(echo $(date))
+
 trap "docker-compose down" SIGINT
 
 cd $HOME/CaroloCup/opendlv.carolocup/docker && make updateDockerBaseImage && make buildIncremental && make createDockerImage && make removeNoneImagesFromDocker &&
 
-cd $cwd && xhost + && docker-compose down && docker-compose up --build >> $(date)log.txt
+cd $cwd && xhost + && docker-compose down && docker-compose up --build >> $d.txt
 
