0f5e86a5e6cc9dcd3f2a0da77f370102c56b1a74
gusshail <gusshail@student.gu.se>
2017-12-09 16:00:29 +0100

    Update Parker.cpp
    And that

--- code/control/parker/src/Parker.cpp
+++ code/control/parker/src/Parker.cpp
@@ -19,6 +19,7 @@
 
 #include "Parker.h"
 
+
 namespace carolocup
 {
 	namespace control
@@ -34,14 +35,19 @@ namespace carolocup
 				  state(Search),
 				  currentSpaceSize(0),
 				  m_foundGaps(),
-				  m_simulator(false) //Set m_simulator to true if simulator is used and false otherwise.
+				  m_simulator(false), //Set m_simulator to true if simulator is used and false otherwise.
+				  m_vehicleControl(),
+				  accumulatedEncoderData(0),
+				  stageProgress(0)
 		{}
 
 		Parker::~Parker()
 		{}
 
 		void Parker::setUp()
-		{}
+		{
+
+		}
 
 		void Parker::tearDown()
 		{}
@@ -52,13 +58,112 @@ namespace carolocup
 			{
 				Container communicationLinkContainer = c.getData<CommunicationLinkMSG>();
 				const CommunicationLinkMSG communicationLinkMSG = communicationLinkContainer.getData<CommunicationLinkMSG>();
+				map<uint32, double> sensors =communicationLinkMSG.getSensors;
+				double ultrasonicSideBack=sensors.get(ID_IN_ULTRASONIC_BACK);
+				double ultrasonicSideFront=sensors.get(ID_IN_ULTRASONIC_SIDE_FRONT);
+				double ultraSonicBack=sensors.get(ID_IN_ULTRASONIC_SIDE_BACK);
+				uint16 distanceToRightLane=communicationLinkMSG.getDistanceToRightLane();
+				map<uint32,double> lidarDistance=communicationLinkMSG.LidarDistance();
+				map<uint32,double> lidarStrength=communicationLinkMSG.LidarStrength();
+				
+				if(this.state==Parking){	
+					vector<double> stages = getDistanceForStages();
+					double turningAngle=90;
+					int8_t direction=2;
+				
+					if(getIdealWheelEncoder(sensors)<=stages.at(i)){
+						if(i%2){	
+							m_vehicleControl.setSteeringWheelAngle(0);
+							m_vehicleControl.setSpeed(1);
+							m_vehicleControl.setLights(ID_OUT_INDICATOR_LF);
+							m_vehicleControl.setLights(ID_OUT_INDICATOR_BF);
+						}else{
+							m_vehicleControl.setSteeringWheelAngle(180);
+							m_vehicleControl.setSpeed(-1);
+							m_vehicleControl.setLights(ID_OUT_INDICATOR_RF);
+							m_vehicleControl.setLights(ID_OUT_INDICATOR_RB);
+						}
+						accumulatedEncoderData+=getIdealWheelEncoder(sensors);		
+					}else{
+						resetEncoders();
+						m_vehicleControl.setSteeringWheelAngle(90);
+						m_vehicleControl.setSpeed(0);
+						if(stages.size()==++stageProgress){
+							this.stateProgress=Parked;
+						}
+					}
+					
+					
+				}else{
+					accumulatedEncoderData+=getIdealWheelEncoder(sensors);
+					if(this.state==Search && ultrasonicSideFront >0){
+						this.state=DiscoveredInitialObject;
+					}else if(this.state==DiscoveredInitialObject && ultrasonicSideFront == 0){
+						this.state==MeasuringParkingSpace;
+						resetEncoders();
+					}else if(this.state==MeasuringParkingSpace && ultrasonicSideFront>0){
+						this.currentSpaceSize=getIdealWheelEncoder(sensors);
+						this.state=Positioning;
+						resetEncoders();
+					}else if(this.state==Positioning && getIdealWheelEncoder(sensors)>=50){
+						this.state=Parking;	
+					}
+				
+				}
+
+				cout<<" Car state "<<this.state<<endl;
+				cout<<" Car stage "<<stage<<endl;
+				cout<<" distance travled "<< getIdealWheelEncoder(sensors)<<endl;
 
 			}
 		}
-
-		vector<double> Parker::getFoundGaps() const {
-			return m_foundGaps;
+		
+		
+		
+		
+		double Parker::getIdealWheelEncoder(map<uint32,double> sensors){
+			double leftEncoder=sensors.get(ID_IN_ENCODER_L);
+			//double rightEncoder=sensors.get(ID_IN_ENCODER_R);
+			//return (leftEncoder+rightEncoder)/2;
+			return (leftEncoder+rightEncoder);
 		}
+		
+		vector<double> Parker::getDistanceForStages(){
+			KeyValueConfiguration kv = getKeyValueConfiguration();
+			double carWidth= kv.getValue<double>("global.car.width");
+			double carLength= kv.getValue<double>("global.car.length");
+			double parkingWidth= kv.getValue<double>("global.parking.width");
+			double laneWidth= kv.getValue<double>("global.lane.width");
+			double ackermannAngle= kv.getValue<double>("global.ackermannAngle");
+			
+			
+			double circleDiameter=	laneWidth/2+parkingWidth/2;
+			double circleCircumpherence=circleDiameter*_Pi;
+			double arcDistance=circleCircumpherence/8;
+			
+			vector<double> stages;
+			stages.push_back(arcDistance);
+			stages.push_back(arcDistance);
+			
+			
+			return stages;
+			
+		}
+		
+		void Parker::resetEncoders(){
+			const ParkerMSG parkerMSG;
+			parker.setReset(1);
+		}
+		
+		
+		double Parker::getLaneWidth(){
+			return 0;
+		}
+		
+		
+		
+
+
 
 
 	}
