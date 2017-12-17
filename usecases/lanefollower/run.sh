c3bac6f91584fa98309d0452bb0a3705190c19d5
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-12-17 17:56:50 +0100

    added ROI

--- usecases/lanefollower/run.sh
+++ usecases/lanefollower/run.sh
@@ -3,20 +3,22 @@
 cwd="$( cd "$(dirname "$0")" ; pwd -P )"
 BUILD=$1
 
-trap "docker-compose down" SIGINT
+trap "docker-compose down --rmi 'all' type --volumes --remove-orphans" SIGINT
 
 if [ -n "${!BUILD}" ]; then
     echo "Building First!!!" >&2
-    cd $HOME/CaroloCup/opendlv.carolocup/docker
-    make updateDockerBaseImage 
+    cd $HOME/CaroloCup/opendlv.carolocup/docker   
     make buildIncremental
     make createDockerImage
-    make removeNoneImagesFromDocker  
+    make removeNoneImagesFromDocker
+    make removeExitedContainers 
 else
     echo "Not Building, Only Running!!!" >&2
 fi
 
-#./findport.sh
+./findport.sh
 
-cd $cwd && xhost + && docker-compose down && docker-compose up --build >> log_data/"$(date +"%Y_%m_%d_%I_%M_%p").txt"
+cd $cwd && xhost + && docker-compose down --rmi 'all' --volumes --remove-orphans &&
+
+docker-compose up --build >> log_data/"$(date +"%Y_%m_%d_%I_%M_%p").txt"
 
